85feb76b82646d9b949e99362ef4667130ff0518
xarxar <67624184+xarxarxar@users.noreply.github.com>
2024-07-07 21:29:43 +0800

    水果花园三消游戏

---
+++ FruitGarden/Assets/Scripts/Libs/AdmobGA.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// 发送数据给谷歌分析
+/// </summary>
+public class AdmobGA : MonoBehaviour
+{
+
+    public static AdmobGA load;     // instance of this class
+    public GoogleAnalyticsV3 GA;    // instance of Google Analytics 
+
+    void Awake()
+    {
+        if (load == null)
+        {
+            // Makes the object target not be destroyed automatically when loading a new scene
+            DontDestroyOnLoad(gameObject); 
+            load = this;
+        }
+        else if (load != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        //// Start Session Google Analytics
+        //GA.StartSession();
+        //// Request banner Google Admob
+        //GoogleMobileAdsScript.advertise.RequestBanner();
+    }
+}
---
+++ FruitGarden/Assets/Scripts/Libs/Ads.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 谷歌广告
+/// </summary>
+public class Ads : MonoBehaviour
+{
+    string ModeName;        // mode of game - Arcede or classic
+    void Start()
+    {
+        //if (PLayerInfo.MODE == 1)
+        //    ModeName = "ARCADE ";
+        //else
+        //    ModeName = "CLASSIC ";
+        //MusicController.Music.BG_play();
+
+        //// check show admob interstitial or no
+        //if (!Timer.timer.isreq)
+        //{
+        //    GoogleMobileAdsScript.advertise.RequestInterstitial();
+        //    Timer.timer.isreq = true;
+        //}
+        //// show banner
+        //GoogleMobileAdsScript.advertise.ShowBanner();
+
+        //// request Google Analytics
+        //AdmobGA.load.GA.LogScreen(ModeName + "Level: " + PLayerInfo.MapPlayer.Level);
+    }
+
+}
