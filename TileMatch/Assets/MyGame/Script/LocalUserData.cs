9a8e29788776fc41f95b9d08ee0e05aeb5e987b3
xarxar <67624184+xarxarxar@users.noreply.github.com>
2024-12-14 01:24:41 +0800

    修复了玩的时候出现的电流声BUG，上传至了onlineFolder01

---
+++ TileMatch/Assets/MyGame/Script/LocalUserData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[SerializeField]
+public class LocalUserData
+{
+    public string UserName;
+    public int CoinCount;
+    public int MaxLifeCount;
+}
