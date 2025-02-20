5bae6b8dcf1c1ba93fa35a86ab6c5cdc613bfeb4
xarxar <67624184+xarxarxar@users.noreply.github.com>
2025-02-20 22:16:18 +0800

    YanglegeyangTest中加入了GUI Pro - Simple CasualPSD v1.0.7

--- YanglegeyangTest/Assets/Layer Lab/Scripts/PanelControl.cs
+++ YanglegeyangTest/Assets/Layer Lab/Scripts/PanelControl.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 #if ENABLE_INPUT_SYSTEM
 using UnityEngine.InputSystem;
@@ -24,10 +21,14 @@ namespace LayerLab.GUIScripts
         [SerializeField] private Button buttonPrev;
         [SerializeField] private Button buttonNext;
 
+        private bool IsOtherMode { get; set; }
+
 
         private void OnValidate()
         {
-            panelTransformDefault = GameObject.Find("Panels").transform;
+            var panels = GameObject.Find("Panels");
+            if (panels) panelTransformDefault = panels.transform;
+            
             buttonPrev = transform.GetChild(0).GetComponent<Button>();
             buttonNext = transform.GetChild(2).GetComponent<Button>();
         }
@@ -37,7 +38,6 @@ namespace LayerLab.GUIScripts
             OnValidate();
         }
 
-        private bool IsOtherMode { get; set; }
         private void Start()
         {
             _textTitle = transform.GetComponentInChildren<TextMeshProUGUI>();
@@ -52,17 +52,16 @@ namespace LayerLab.GUIScripts
             defaultPanels[_page].SetActive(true);
             
             
+            if(panelTransformOther == null) return;
             
-            
-            if (otherPanels.Count > 0)
+            foreach (Transform t in panelTransformOther)
             {
-                foreach (Transform t in panelTransformOther)
-                {
-                    otherPanels.Add(t.gameObject);
-                    t.gameObject.SetActive(false);
-                }
-                otherPanels[_page].SetActive(true);
+                otherPanels.Add(t.gameObject);
+                t.gameObject.SetActive(false);
             }
+            
+            
+            if (otherPanels.Count > 0) otherPanels[_page].SetActive(true);
 
             
             
@@ -71,7 +70,7 @@ namespace LayerLab.GUIScripts
             CheckControl();
         }
 
-        void Update()
+        private void Update()
         {
             if (defaultPanels.Count <= 0 || !_isReady) return;
 
@@ -98,7 +97,7 @@ namespace LayerLab.GUIScripts
 
         //Click_Prev
         //Click_Prev
-        public void Click_Prev()
+        private void Click_Prev()
         {
             if (_page <= 0) return;
 
@@ -125,7 +124,7 @@ namespace LayerLab.GUIScripts
         }
 
         //Click_Next
-        public void Click_Next()
+        private void Click_Next()
         {
             if (_page >= defaultPanels.Count - 1) return;
             
@@ -139,7 +138,7 @@ namespace LayerLab.GUIScripts
         }
 
 
-        void SetArrowActive()
+        private void SetArrowActive()
         {
             buttonPrev.gameObject.SetActive(_page > 0);
             buttonNext.gameObject.SetActive(_page < defaultPanels.Count - 1);
@@ -170,10 +169,10 @@ namespace LayerLab.GUIScripts
             CheckControl();
         }
         
-        void SetMode()
+        private void SetMode()
         {
-            panelTransformDefault.gameObject.SetActive(IsOtherMode);
-            if(otherPanels.Count > 0) panelTransformOther.gameObject.SetActive(!IsOtherMode);
+            panelTransformDefault.gameObject.SetActive(!IsOtherMode);
+            if(otherPanels.Count > 0) panelTransformOther.gameObject.SetActive(IsOtherMode);
         }
     }
 }
