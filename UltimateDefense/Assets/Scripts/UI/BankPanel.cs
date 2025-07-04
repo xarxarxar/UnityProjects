8385e6699e04a61d83b33fbe4d3a214d053d87aa
xarxar <67624184+xarxarxar@users.noreply.github.com>
2025-07-04 14:12:42 +0800

    修改了银行界面的打开关闭效果，修改了科技界面和升级炮塔界面的UI

--- UltimateDefense/Assets/Scripts/UI/BankPanel.cs
+++ UltimateDefense/Assets/Scripts/UI/BankPanel.cs
@@ -1,26 +1,26 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BankPanel : MonoBehaviour
+public class BankPanel : BasePanel
 {
     [SerializeField] private Text _bankCoinText;
-    [SerializeField] private Button _closeButton;//关闭当前面板的按钮
 
-    private void OnEnable()
+    public override void OnEnable()
     {
-        _closeButton.onClick.AddListener(CloseButton);
-       BattleManager.Instance.PauseGame();//暂停游戏
+        base.OnEnable();
+        CloseButton.onClick.AddListener(CloseButtonClick);
+        //BattleManager.Instance.PauseGame();//暂停游戏
         _bankCoinText.text = $"当前存款:{BankManager.Instance.Count}";
     }
 
     private void OnDisable()
     {
-        BattleManager.Instance.ResumeGame();//恢复游戏
-        _closeButton.onClick.RemoveAllListeners();
+        //BattleManager.Instance.ResumeGame();//恢复游戏
+        CloseButton.onClick.RemoveAllListeners();
     }
 
     //关闭当前面板
-    private void CloseButton()
+    private void CloseButtonClick()
     {
         BattleUIManager.Instance.HidewBankPanel();
     }
